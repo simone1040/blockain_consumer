bc002572084641135d67b8b71ff52e6481881dea
simone <simone1040@hotmail.it>
2020-02-01 20:52:56 +0100

    aggiunto calcolo proof of work

--- src/main/java/com/simone/progetto/Block.java
+++ src/main/java/com/simone/progetto/Block.java
@@ -1,10 +1,19 @@
 package com.simone.progetto;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Random;
+
 public class Block {
     private Transaction data;
     private String hash;
+    private Random random = new Random();
     private String previousHash;
     private long timestamp;
+    private static final Integer LOW_SECOND = 1000;
+    private static final Integer HIGH_SECOND = 4500;
+    private static final Logger log = LoggerFactory.getLogger(Block.class);
 
     public Block(Transaction data, String previousHash) {
         this.data = data;
@@ -17,7 +26,20 @@ public class Block {
         if(this.previousHash != null){
             stringToHash = this.previousHash + timestamp + data.getStringToHash();
         }
+        this.computeProofOfWoork();
         return Utils.applySha256(stringToHash);
+    }
+
+    private void computeProofOfWoork(){
+        int mseconds = (random.nextInt(HIGH_SECOND-LOW_SECOND) + LOW_SECOND);
+        try{
+            Thread.sleep(mseconds );
+            log.info("Tempo usato calcolare il proof of work in ms --> " + mseconds);
+        }
+        catch (InterruptedException ex){
+            log.info("Eccezione nello sleep --> " + ex);
+        }
+
     }
 
     public Transaction getData() {
--- src/main/java/rabbitMQ/RabbitMQConfiguration.java
+++ src/main/java/rabbitMQ/RabbitMQConfiguration.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RabbitMQConfiguration {
 
-	public static final String DEFAULT_PARSING_QUEUE = "default_transaction_q";
+	public static final String DEFAULT_PARSING_QUEUE = "default_transaction1_q";
 
 	@Bean
 	public Queue defaultParsingQueue() {
